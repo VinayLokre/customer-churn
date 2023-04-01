f7167699427d638c75e0e0f096ef89f9c392cbd0
VinayLokre <90213620+VinayLokre@users.noreply.github.com>
2023-04-01 21:51:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import streamlit as st
-import pandas as pd
-import joblib
-from sklearn.preprocessing import LabelEncoder
+#import streamlit as st
+#import pandas as pd
+#import joblib
+#from sklearn.preprocessing import LabelEncoder
 
 st.title("CUSTOMER CHURN PREDICTION APPLICATION")
 
